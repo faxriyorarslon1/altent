3a21d53d180405b1ba65f5cb0b90ce3cb4b885b5
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-01-29 22:01:04 +0500

    add postgresql conf

--- altent/settings.py
+++ altent/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'altent.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'altent',
+        'USER': 'altentuser',
+        'PASSWORD': 'altentpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
